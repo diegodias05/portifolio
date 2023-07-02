8b5045d6d5381bccc007fcae0cb2d930830fe5b5
diegodias05 <diegobr.00990@outlook.com>
2023-07-01 21:13:05 -0300

    adicionando about

--- scrypts/index.js
+++ scrypts/index.js
@@ -1,11 +1,19 @@
 const htmlElement = document.querySelector('html');
 const toggleSwitch = document.getElementById('switch');
 
-toggleSwitch.addEventListener('change', function() {
-    if (this.checked) {
-        htmlElement.classList.add('dark-mode');
+const storedMode = localStorage.getItem('darkMode');
+if (storedMode === 'true') {
+  toggleSwitch.checked = true;
+  htmlElement.classList.add('dark-mode');
+}
 
-    } else {
-        htmlElement.classList.remove('dark-mode');
-    }
+toggleSwitch.addEventListener('change', function() {
+  if (this.checked) {
+    htmlElement.classList.add('dark-mode');
+    localStorage.setItem('darkMode', 'true');
+  } else {
+    htmlElement.classList.remove('dark-mode');
+    localStorage.setItem('darkMode', 'false');
+  }
+  
 });
